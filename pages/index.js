96b131f0cc75962b00d5ce74f3faf4d304fb249c
ciliumdev <68327832+ciliumgroupdev@users.noreply.github.com>
2020-12-26 23:41:38 +0100

    update indext.js
    pushing new blog

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ import { getAllPostsForHome } from '@/lib/api'
 import Head from 'next/head'
 import { CMS_NAME } from '@/lib/constants'
 
+
+
+
 export default function Index({ allPosts }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
