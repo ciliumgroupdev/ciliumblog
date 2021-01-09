496344237bf674c759248460f3a699ac627ccb6a
ciliumdev <68327832+ciliumgroupdev@users.noreply.github.com>
2021-01-09 08:24:35 +0100

    updateindex
    fix new code

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,6 @@ import Head from 'next/head'
 import { CMS_NAME } from '@/lib/constants'
 
 
-
-
 export default function Index({ allPosts }) {
   const heroPost = allPosts[0]
   const morePosts = allPosts.slice(1)
@@ -43,4 +41,5 @@ export async function getStaticProps({ preview }) {
   return {
     props: { allPosts },
   }
+  
 }
